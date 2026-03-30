180bddc434c98678e4e382251f5175aeef7f9bfa
harsirat757 <harsiratsingh920@gmail.com>
2026-03-30 19:37:37 +0530

    updating API address

--- src/API/dataAPI.jsx
+++ src/API/dataAPI.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default async function dataAPI(data, setDiseaseResult) {
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    
+
+    const response = await fetch("http://35.154.214.66/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
